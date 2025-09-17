2ef1e296b2b8c9cd7d1d7d167116ce1e405b7e95
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2025-09-17 16:39:43 +0800

    feat: add necessary nvim

---
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,20 @@
+return {
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			biome = {},
+			lua_ls = {},
+			html = {},
+			cssls = {},
+			tsserver = {},
+			clangd = {},
+			terraformls = {},
+			astro = {},
+			eslint = {},
+			golangci_lint_ls = {},
+			pyright = {},
+			elixirls = {},
+			rust_analyzer = {},
+		},
+	},
+}
